d239babd5511a2d4ff34dd93129f9977ffdddb88
AhmadBilalB <ahmad.bahadur.00@gmail.com>
2025-03-02 17:06:07 +0500

    Added Spring Boot project files

---
+++ src/main/java/com/example/Translation_Management_Service/TranslationManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Translation_Management_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Translation_Management_Service/TranslationManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Translation_Management_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
